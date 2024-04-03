746826d926a049ec5ef4bdda81ca8a7d2ace1a01
Volodymyr Derunov <vderunov@gmail.com>
2024-04-03 11:44:45 +0300

    Add deployment

---
+++ generateDeployments.js
@@ -0,0 +1,45 @@
+const fs = require('fs').promises;
+const path = require('path');
+const ethers = require("ethers");
+
+const chainId = '11155420'; // OP Sepolia Chain ID
+
+async function readJSON(filePath) {
+    return JSON.parse(await fs.readFile(filePath, 'utf8'));
+}
+
+async function writeJSON(filePath, data) {
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
+}
+
+function readableAbi(abi) {
+    return new ethers.Interface(abi).format();
+}
+
+async function getAddressForDeployment() {
+    const deploymentData = await readJSON(path.join(__dirname, 'deployment.json'));
+    return deploymentData[chainId]['Whitelist'];
+}
+
+async function processFiles() {
+    await fs.mkdir(`deployments/${chainId}/`, {recursive: true})
+    const data = await readJSON(path.join(__dirname, '/out/Whitelist.sol/Whitelist.json'));
+    const address = await getAddressForDeployment();
+    const abi = readableAbi(data.abi);
+
+    await writeJSON(`deployments/${chainId}/Whitelist.abi.json`, data.abi);
+    await writeJSON(`deployments/${chainId}/Whitelist.meta.json`, {
+        compiler: data.metadata.compiler,
+        settings: {
+            optimizer: data.metadata.settings.optimizer,
+            evmVersion: data.metadata.settings.evmVersion
+        }
+    });
+    await writeJSON(`deployments/${chainId}/Whitelist.methods.json`, data.methodIdentifiers);
+    await writeJSON(`deployments/${chainId}/Whitelist.readable-abi.json`, abi);
+    await writeJSON(`deployments/${chainId}/Whitelist.address.json`, address);
+    await fs.writeFile(`deployments/${chainId}/Whitelist.js`, `module.exports.address = "${address}";`, 'utf8');
+    await fs.appendFile(`deployments/${chainId}/Whitelist.js`, `\nmodule.exports.abi = ${JSON.stringify(abi, null, 2)};`, 'utf8');
+}
+
+processFiles().catch(console.error);
